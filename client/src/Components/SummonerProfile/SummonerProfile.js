0d9696e8c9897bb8eef4b627c8fa4874999991fb
Seb <ra3gan1991@gmail.com>
2020-04-07 02:34:58 +0200

    Se implementa handler para sacar datos de Summoner

--- client/src/Components/SummonerProfile/SummonerProfile.js
+++ client/src/Components/SummonerProfile/SummonerProfile.js
@@ -1,11 +1,66 @@
 import React, {Component} from 'react';
+import RiotApiComponent from '../RiotApiHandler/RiotApiComponent';
 const ddEndpoint = "http://ddragon.leagueoflegends.com/cdn/10.7.1/img/profileicon/";
 class SummonerProfile extends Component{
+	constructor(props){
+  	super(props);	
+  	this.state={
+  		ranked:{},
+  		loadedRanked:false
+  	}
+ 
+		}
+
+	componentDidMount(){
+		
+		const leagueCall = async()=>{
+
+		await RiotApiComponent.getRanked(this.props.summonerData.id)
+		.then(d=>{
+			console.log("RANKED DATA",d); 
+			this.setState({
+			ranked: d.data,
+			loadedRanked:true
+		});
+			
+		})
+		.catch(e=>{console.log(e);});}
+		leagueCall();
+		
+		
+	}
 	
 	render(){
 		const data = this.props.summonerData;
-		console.log(data.profileIconId)
 		const imgProfile = ddEndpoint+data.profileIconId+".png";
+		const setupLeague = (load,state)=>{
+			console.log("LOAD",load);
+			console.log("STATE",state)
+			if(load){
+				return (<div><p>Solo: {renderLeague("solo",state)}</p> <p>Flex: {renderLeague("flex",state)}</p></div>)
+			}
+		}
+		const renderLeague = (league,state)=>{
+			switch(league){
+				case "solo":{
+					if(state.ranked.unranked.solo){
+						return "Unranked"
+					}else{
+						const soloQStr = state.ranked.soloq.tier +" "+state.ranked.soloq.rank
+						return soloQStr
+					}
+				}
+				case "flex":{
+					if(state.ranked.unranked.flex){
+						return "Unranked"
+					}else{
+						const soloQStr = state.ranked.flexq.tier +" "+state.ranked.flexq.rank
+						return soloQStr
+					}
+				}
+				
+			}
+		}
 		
 		return (
 				<div className="row">
@@ -16,6 +71,9 @@ class SummonerProfile extends Component{
 						<h2 className="text-center summLevel">{data.summonerLevel}</h2>
 						<br/>
 						<h1 className="text-center summName">{data.name}</h1>
+						{setupLeague(this.state.loadedRanked,this.state)}
+						
+						
 					</div>
 					
 					
