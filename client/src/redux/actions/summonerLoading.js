a03024d4bf05fa554c79c7ce6e3d5af03a1b1398
Seb <ra3gan1991@gmail.com>
2020-04-09 01:58:17 +0200

    Cambio de nombre y estructura

---
+++ client/src/redux/actions/summonerLoading.js
@@ -0,0 +1,25 @@
+import types from '../types';
+
+const typeLoading = types.summonerLoading;
+const typeLoaded = types.summLoaded;
+
+const summonerLoading = (x,t) => {
+switch (t){
+	case"loaded":{
+		 return {
+   		type: typeLoaded,
+  		payload: x
+  }
+	}
+	case"loading":{
+		 return {
+         type: typeLoading,
+         payload: x
+  }
+	}
+}
+ 
+
+}
+
+export default summonerLoading;
